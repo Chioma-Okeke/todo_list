608492392007f1cbbd3e5f16c6b99276aa88817d
Okeke Chioma <okekegeraldenne@gmail.com>
2023-03-20 22:03:57 +0100

    first commit

---
+++ index.js
@@ -0,0 +1,26 @@
+document.querySelector('#push').onclick = function(){
+    if(document.querySelector('#list-input input').value.length == 0){
+        alert("Kindly Enter Task Name!!!!")
+    }
+
+    else{
+        document.querySelector('#tasks').innerHTML += `
+            <div class="task">
+                <span id="taskname">
+                    ${document.querySelector('#list-input input').value}
+                </span>
+                <button class="delete">
+                    <img src="./images/5299154331543238955.svg" alt="">
+                    <i class="far fa-trash-alt"></i>
+                </button>
+            </div>
+        `;
+
+        var current_tasks = document.querySelectorAll(".delete");
+        for(var i=0; i<current_tasks.length; i++){
+            current_tasks[i].onclick = function(){
+                this.parentNode.remove();
+            }
+        }
+    }
+}
